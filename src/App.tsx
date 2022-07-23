886b3ce63ff676c4b393f914050edc772a40a0b4
Tomas M <tmisiuk@react.vslabs.com>
2022-07-23 17:42:52 +0300

    Added firebase, created Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { CssBaseline } from "@mui/material";
+import { Header } from "./components";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Provider store={store}>
         <CssBaseline />
-        <Box
-          height="100vh"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-        >
-          <Paper
-            elevation={3}
-            sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
-          >
-            <Typography color="primary.dark" variant="h1">
-              Starter App
-            </Typography>
-          </Paper>
-        </Box>
         <Routes>
+          <Route path="/" element={<Header />} />
           <Route path="/new-component" element={<div>New component</div>} />
         </Routes>
       </Provider>
     </Router>
   );
-}
+};
 
 export default App;
