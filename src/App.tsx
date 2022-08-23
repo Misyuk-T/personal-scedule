97ab10a3feb98f54cfa524c9ac3ca7061c33e403
Tomas M <tmisiuk@react.vslabs.com>
2022-08-23 14:28:07 +0300

    Install Yup. Added new storage rules to firebase.rules file. Added new components to App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import store from "./redux/store";
 import { Provider } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { CssBaseline } from "@mui/material";
-import { Header } from "./components";
+
+import { Header, PrivateRoute, PageLoader } from "./components";
 import Profile from "./pages/Profile";
+import store from "./redux/store";
 
 const App = () => {
   return (
     <Router>
       <Provider store={store}>
         <CssBaseline />
+        <PageLoader />
         <Header />
+
         <Routes>
           <Route path="/" element={<p>HOME PAGE</p>} />
-          <Route path="/profile" element={<Profile />} />
+
+          <Route element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Provider>
     </Router>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { Header } from "./Header";
 export { AccountProfile } from "./AccountProfile";
 export { AccountInformation } from "./AccountInformation";
+export { TextField } from "./TextField";
+export { Alert } from "./Alert";
+export { PrivateRoute } from "./PrivateRoute";
+export { PageLoader } from "./PageLoader";
